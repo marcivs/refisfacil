c8c42bd0aed85c5beef969e5eabe2828734cc274
marcivs <marcivs@gmail.com>
2017-07-31 17:23:13 -0400

    Concluida a Barra de Navegação concluido o carousel concluido os thumbnails concluidos os anuncios concluido o footer pagina principal concluida com links instalado o allauth configurações de login, signup e signout urls link atualizados

--- src/profiles/urls.py
+++ src/profiles/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from . import views
 
 
 urlpatterns = [
-
+    url(r'^accounts/', include('allauth.urls')),
 ]
--- src/refisfacil/urls.py
+++ src/refisfacil/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import url, include
 from django.contrib import admin
 
@@ -21,8 +23,15 @@ from . import views as home_views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', home_views.home, name='home'),
+    #url(r'^accounts/', include('allauth.urls')),
 ]
 
 urlpatterns += [
     url(r'^profiles/', include('profiles.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
